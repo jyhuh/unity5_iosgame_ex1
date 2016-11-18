a7123854ada0e4291b90df71c47a895c571ca346
hjy2306 <hjy2306@naver.com>
2016-11-19 00:23:36 +0900

    Day01_illumiBalls
    Light, collider, script, and key input

---
+++ Assets/Script/GravityController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GravityController: MonoBehaviour {
+
+	const float Gravity = 9.81f;
+
+	public float gravityScale = 1.0f;
+
+	// Update is called once per frame
+	void Update () {
+	
+		Vector3 vector = new Vector3 ();
+
+		vector.x = Input.GetAxis ("Horizontal");
+		vector.z = Input.GetAxis ("Vertical");
+
+		if (Input.GetKey ("z")) {
+			vector.y = 1.0f;
+		} else {
+			vector.y = -1.0f;
+		}
+
+		Physics.gravity = Gravity * vector.normalized * gravityScale;
+
+	}
+}
